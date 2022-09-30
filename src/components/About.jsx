1709c7f7de0e2a3a8b3d4573e0b42b5fbe3b1e1e
illunaaa-PA <71630552+illunaaa-PA@users.noreply.github.com>
2022-09-30 21:46:43 +0800

    project done

---
+++ src/components/About.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const About = () => {
+    return (
+
+        <div name="about" className="w-full h-screen bg-gradient-to-b from-gray-800
+        to-black text-white">
+
+            <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+
+                <div className="pb-8">
+                    <p className="text-4xl font-bold inline border-b-4 border-gray-500">About</p>
+                </div>
+
+                <p className="text-xl mt-20">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nostrum numquam corporis quidem maiores minus amet quod, ipsam ab?
+                    Tenetur ipsam corporis voluptatum vitae? Eos architecto culpa reiciendis repellendus. In, voluptate. Quidem necessitatibus,
+                    quos tempore eveniet excepturi dolor ipsum. Omnis quo sunt delectus fugit, odio corporis alias eum ducimus tempore cum.
+                </p>
+
+                <br />
+
+                <p className="text-xl">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, fugiat! Sequi, veniam, maiores hic molestiae
+                    pariatur animi iusto sit magnam tenetur qui dolore error eaque a officiis cumque cum corporis voluptate praesentium,
+                    numquam vitae ratione soluta! Excepturi repellat architecto error dolores animi nulla impedit suscipit asperiores,
+                    in recusandae. Exercitationem, alias.
+                </p>
+
+            </div>
+
+        </div>
+    );
+};
+
+export default About;
